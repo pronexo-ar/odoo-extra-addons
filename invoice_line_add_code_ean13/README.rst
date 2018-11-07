e229b8df6ba844e85fa4a4f205ed1a449141639a
pronexo <jm@pronexo.com>
2018-11-06 22:39:57 -0300

    8.0

---
+++ invoice_line_add_code_ean13/README.rst
@@ -0,0 +1,29 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :alt: License: AGPL-3
+
+Invoice Line Add Product Ean13
+=====================================
+Add product ean13 code field on account_invoice line linked to product_id.ean13
+
+Bug Tracker
+===========
+Bugs are tracked on `GitHub Issues <https://github.com/pronexo-odoo/invoice_line_add_code_ean13/issues>`_.
+
+Credits
+=======
+
+Author:
+* Juan Manuel De Castro (pronexo.com)
+
+
+Maintainer
+==========
+.. image:: http://www.pronexo.com/downloads/company_logo.png
+:alt: pronexo.com
+   :target: https://www.pronexo.com
+
+This module is maintained by the pronexo.com.
+
+
+
+
---
+++ sale_line_add_code_ean13/models/sale_order_line.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2018 PRONEXO (<https://www.pronexo.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import logging
+from openerp import models, fields, api, _
+
+_logger = logging.getLogger(__name__)
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    product_ean13_code = fields.Char(
+            compute="_get_product_info",
+            string="EAN",
+            store=True
+    )
+
+    @api.one
+    @api.depends('product_id','product_id.ean13')
+    def _get_product_info(self):
+        if self.product_id:
+            self.product_ean13_code = self.product_id.ean13
+
